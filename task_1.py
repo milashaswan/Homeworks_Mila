2bf326be46e7324dbe61f8157e9044b6625b3e48
milashaswan <milashaswan323@gmail.com>
2022-07-08 15:13:42 +0300

    задание 1 урок 2

--- task_1.py
+++ task_1.py
@@ -1,4 +1,4 @@
-my_list = [77, -77, 'True', True, 9.5,  None, 'list']
+my_list = [77, -77, False, 0,  'True', True, 9.5,  None, 'list']
 def my_type(el):
     for el in range(len(my_list)):
         print(type(my_list[el]))
