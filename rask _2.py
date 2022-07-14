fa75e68811888671d204b4667735125201f76507
milashaswan <milashaswan323@gmail.com>
2022-07-14 15:37:44 +0300

    урок 3 задача 2

---
+++ rask _2.py
@@ -0,0 +1,7 @@
+def user (**data_user):
+    for i in data_user:
+        print(i)
+    print(data_user)
+
+
+user(name= "Ivan" , lastname= "Semenov", age= "30", city="Kiev", email="semenov@gmail.com", number="88877755")
